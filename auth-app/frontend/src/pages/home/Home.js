065c0dcdda60a06bfd12a2b2c244d5b531572fdb
tpthanh2006 <tranpthanh2006@gmail.com>
2025-01-22 20:33:37 -0600

    build home page

--- auth-app/frontend/src/pages/home/Home.js
+++ auth-app/frontend/src/pages/home/Home.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
+import "./Home.scss";
+import loginImg from "../../assets/login.svg";
 
 const Home = () => {
   return (
     <div>
       <Header/>
-      <h1>Home</h1>
+      <section className='container hero'>
+        <div className="hero-text">
+          <h2>Ultimate MERN Stack Authentication System</h2>
+          <p>Learn and Master Authentication & Authorization using MERN Stack</p>
+          <p>Implement User Registration, Login, Password Reset, Social Login, User Permission, Email Notifications, etc.</p>
+          <div className='hero-buttons --flex-start'>
+            <button className='--btn --btn-danger'>
+              Register
+            </button>
+
+            <button className='--btn --btn-primary'>
+              Login
+            </button>
+          </div>
+        </div>
+
+        <div className="hero-image">
+          <img src={loginImg} alt="Auth"/>
+        </div>
+      </section>
+      <Footer/>
     </div>
   )
 }
