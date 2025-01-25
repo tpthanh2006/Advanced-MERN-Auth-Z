646e7fb0eb99b483ccb1b48dc432a58f46d612f2
tpthanh2006 <tranpthanh2006@gmail.com>
2025-01-25 14:11:21 -0600

    complete login layout

--- auth-app/frontend/src/App.js
+++ auth-app/frontend/src/App.js
@@ -1,12 +1,21 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import Home from "./pages/home/Home";
+import Login from "./pages/auth/Login";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>} /> 
+          <Route path="/" element={
+              <Home />
+            }
+          /> 
+          <Route path="/login" element={
+            <Login />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
