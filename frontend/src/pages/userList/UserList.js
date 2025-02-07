e1353bdcb7e2acf38613e38b727a4aae9c5d2323
tpthanh2006 <tranpthanh2006@gmail.com>
2025-02-07 16:59:27 -0600

    90%

--- frontend/src/pages/userList/UserList.js
+++ frontend/src/pages/userList/UserList.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { FaTrash } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import ChangeRole from "../../components/changeRole/ChangeRole";
+import { Spinner } from "../../components/loader/Loader";
+import PageMenu from "../../components/pageMenu/PageMenu";
+import Search from "../../components/search/Search";
+import UserStats from "../../components/userStats/UserStats";
+import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
+import { getUsers } from "../../redux/features/auth/authSlice";
 import "./UserList.scss";
-import PageMenu from '../../components/pageMenu/PageMenu';
-import UserStats from '../../components/userStats/UserStats';
-import Search from '../../components/search/Search';
-import { FaTrash } from 'react-icons/fa';
-import ChangeRole from '../../components/changeRole/ChangeRole';
+//import { confirmAlert } from "react-confirm-alert";
+//import "react-confirm-alert/src/react-confirm-alert.css";
+/*import {
+  FILTER_USERS,
+  selectUsers,
+} from "../../redux/features/auth/filterSlice";*/
+//import ReactPaginate from "react-paginate";
 
 const UserList = () => {
+  useRedirectLoggedOutUser("/login");
+  const dispatch = useDispatch();
+
+  const { users, isLoading, isLoggedIn, isSuccess, message} = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch])
+
   return <section>
     <div className="container">
       <PageMenu />
-      <UserStats />
+      {/*<UserStats />*/}
 
       <div className="user-list">
         <div className="table">
